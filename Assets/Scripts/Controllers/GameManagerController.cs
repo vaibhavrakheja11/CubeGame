a843cb40a214d5224c17a5e35379a17b36275b2a
vaibhavrakheja11 <vaibhavrakheja11@gmail.com>
2021-03-14 12:24:54 -0400

    Initial Commit

---
+++ Assets/Scripts/Controllers/GameManagerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerController : MonoBehaviour
+{
+
+    public PanelManager uiManager;
+
+    public GameController gameController;
+
+
+    #region SINGLETON PATTERN
+    private static GameManagerController _instance;
+    public static GameManagerController Instance
+    {
+        get {
+            if (_instance == null)
+            {
+                _instance = GameObject.FindObjectOfType<GameManagerController>();
+            }
+        
+            return _instance;
+        }
+    }
+    #endregion
+}
